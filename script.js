b4c71e8cbacc58f24e640a6b1484f8fa4559ed46
Law-js <hp.hectorpascal@gmail.com>
2022-02-16 17:56:15 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+const isPalindrome = (str) => {
+  const smallStr = str.value.toLowerCase().replace(/[\W_]/g, '');
+  const reversed = smallStr.split('').reverse().join('');
+
+  if (reversed === smallStr) console.log('Is a Palindrome');
+  else console.log('Is not a Palindrome');
+};
+
+button.addEventListener('click', () => isPalindrome(input));
